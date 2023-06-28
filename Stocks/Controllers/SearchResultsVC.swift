19af2aa367b42e4ba49f27b6d21def669077b079
meruyert123 <meruertzhunusova1711@gmail.com>
2023-06-28 21:11:14 +0600

    Add table view in search results vc

--- Stocks/Controllers/SearchResultsVC.swift
+++ Stocks/Controllers/SearchResultsVC.swift
@@ -1,10 +1,47 @@
 import UIKit
 
 class SearchResultsVC: UIViewController {
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        // Register cell
+        table.register(SearchResultTableViewCell.self,
+                       forCellReuseIdentifier: SearchResultTableViewCell.identifier)
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .red
+        setupTable()
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    private func setupTable() {
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+}
+
+extension SearchResultsVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: SearchResultTableViewCell.identifier, for: indexPath)
+        
+        cell.textLabel?.text = "AAPL"
+        cell.detailTextLabel?.text = "Apple"
+        
+        return cell
+    }
+    
 }
