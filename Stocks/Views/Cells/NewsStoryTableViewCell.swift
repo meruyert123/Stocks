bd724721d6b647a59b47461f02839aca9064d477
meruyert123 <meruertzhunusova1711@gmail.com>
2023-06-30 12:54:42 +0600

    Update font size in newscell

--- Stocks/Views/Cells/NewsStoryTableViewCell.swift
+++ Stocks/Views/Cells/NewsStoryTableViewCell.swift
@@ -30,7 +30,7 @@ class NewsStoryTableViewCell: UITableViewCell {
     // Headline
     private let headlineLabel: UILabel = {
         let label = UILabel()
-        label.font = .systemFont(ofSize: 24, weight: .regular)
+        label.font = .systemFont(ofSize: 22, weight: .regular)
         label.numberOfLines = 0
         return label
     }()
@@ -68,9 +68,9 @@ class NewsStoryTableViewCell: UITableViewCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        let imageSize: CGFloat = contentView.height - 6
+        let imageSize: CGFloat = contentView.height/1.4
         storyImageView.frame = CGRect(x: contentView.width - imageSize - 10,
-                                      y: 3,
+                                      y: (contentView.height - imageSize) / 2,
                                       width: imageSize,
                                       height: imageSize)
         
